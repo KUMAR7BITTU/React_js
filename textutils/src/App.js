11d8c34d317991899c523fbfc2274a696618897e
KUMAR7BITTU <bittukumar30dec2002@gmail.com>
2023-08-09 21:58:59 +0530

    updated App.js and App.css

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -3,25 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <>
-    <h1>This is me</h1>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React With Bittu
-        </a>
-      </header>
-    </div>
-    </>
+    <div className="blank">Lovely</div>
   );
 }
 
