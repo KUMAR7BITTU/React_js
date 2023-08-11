0d875e67edd8b31186991c3cfa51edd4beacdf9f
KUMAR7BITTU <bittukumar30dec2002@gmail.com>
2023-08-11 06:32:49 +0530

    setText added

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const handleUpClick = ()=>{
+    console.log("Uppercase was clicked");
+    setText("You have clicked on handleUpClick")
+  }
+  const handleOnChange = ()=>{
+    console.log("On change");
+  }
   const [text,setText] = useState('Enter text here2');
+  // text = "new text"; // Wrong way to change the text
+  // setText("new text"); //correct way to change the text
   return (
     <div>
-      <h1>{props.heading} - {text}</h1>
+      <h1>{props.heading}</h1>
       <div className="mb-3">
         <textarea
-          className="form-control"
+          className="form-control" value={text} onChange={handleOnChange}
           id="myBox"
           rows="8"
         ></textarea>
       </div>
-      <button className="btn btn-primary">Convert to Uppercase</button>
+      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
     </div>
   );
 }
