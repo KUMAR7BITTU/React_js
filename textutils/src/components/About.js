e7fc949ae38402222ec441421c0c2dad187f917b
KUMAR7BITTU <bittukumar30dec2002@gmail.com>
2023-08-11 18:34:55 +0530

    border added in dark mode

--- textutils/src/components/About.js
+++ textutils/src/components/About.js
@@ -12,7 +12,8 @@ export default function About() {
     if(myStyle.color == 'black'){
       setMyStyle({
         color:'white',
-        backgroundColor:'black'
+        backgroundColor:'black',
+        border:'1px solid white'
       })
       setBtnText("Enable Light Mode")
     }
