7e99ad3ed9463589d250a7c47588cc7902f3aeb3
KUMAR7BITTU <bittukumar30dec2002@gmail.com>
2023-08-11 15:48:48 +0530

    clear button added

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -12,11 +12,17 @@ export default function TextForm(props) {
     let newText = text.toLowerCase();
     setText(newText)
   }
+
+  const handleClearClick = ()=>{
+    //console.log("Uppercase was clicked" + text);
+    let newText = '';
+    setText(newText)
+  }
   const handleOnChange = (event)=>{
     //console.log("On change");
     setText(event.target.value);
   }
-  const [text,setText] = useState('Enter text here2');
+  const [text,setText] = useState('');
   // text = "new text"; // Wrong way to change the text
   // setText("new text"); //correct way to change the text
   return (
@@ -32,9 +38,10 @@ export default function TextForm(props) {
       </div>
       <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
       <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
     </div>
     <div className="container my-3">
-      <h1>Your text summary</h1>
+      <h2>Your text summary</h2>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p>{0.008 * text.split(" ").length} Minutes read</p>
       <h2>Preview</h2>
