a65ceb3b56002b8e2725ecc25f2f44d5b0b38175
KUMAR7BITTU <bittukumar30dec2002@gmail.com>
2023-08-11 08:54:25 +0530

    project updated

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -6,6 +6,12 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText)
   }
+
+  const handleLoClick = ()=>{
+    //console.log("Uppercase was clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText)
+  }
   const handleOnChange = (event)=>{
     //console.log("On change");
     setText(event.target.value);
@@ -24,11 +30,15 @@ export default function TextForm(props) {
           rows="8"
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
     </div>
     <div className="container my-3">
       <h1>Your text summary</h1>
-      <p>3432 words and 454934594 characters</p>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes read</p>
+      <h2>Preview</h2>
+      <p>{text}</p>
     </div>
     </>
   );
