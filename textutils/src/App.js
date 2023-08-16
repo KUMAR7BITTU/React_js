1b361d56760890a1e390a3b48e5dce9c1e2d711d
KUMAR7BITTU <bittukumar30dec2002@gmail.com>
2023-08-16 19:20:15 +0530

    add background color

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -9,10 +9,20 @@ import { useSyncExternalStore } from "react";
 function App() {
   const [mode,setMode] = useState('light');// This is my state variable which tells me whether dark mode is enabled or not .
   
+  const toggleMode = () =>{
+    if(mode==='light'){
+      setMode('dark')
+      document.body.style.backgroundColor = 'grey';
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
       {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-      <Navbar title="TextUtils" mode={mode}/>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
       {/* <Navbar/> */}
       <div className="container my-3">
       <TextForm heading="Enter the text to analyze below"/> 
--- textutils/src/components/Navbar.js
+++ textutils/src/components/Navbar.js
@@ -43,6 +43,11 @@ export default function Navbar(props) {
               Search
             </button>
           </form> */}
+          <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+          <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
+          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
+</div>
+
         </div>
       </div>
     </nav>
